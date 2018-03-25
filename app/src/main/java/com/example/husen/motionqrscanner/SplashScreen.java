425263321126a852b5e7fecd291b93c51dc7773e
husen <husentod10@gmail.com>
2018-03-26 06:17:42 +0700

    update fix bug saat deploy

--- app/src/main/java/com/example/husen/motionqrscanner/SplashScreen.java
+++ app/src/main/java/com/example/husen/motionqrscanner/SplashScreen.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
@@ -18,21 +19,15 @@ public class SplashScreen extends AppCompatActivity {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         setContentView(R.layout.activity_splash_screen);
 
         getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable() {
-
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
                 Intent i = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(i);
 
